61b0d122d4f9e907010ee60b6de2e3ed60e11287
Ahmet Berke Tekin <ahmetberketekin@gmail.com>
2023-11-15 13:44:46 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+const myLogger =(req, res, next)=>{
+  console.log('Middleware Log1');
+  next();
+}
+
+const myLogger2 =(req, res, next)=>{
+  console.log('Middleware Log2');
+  next();
+}
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo name',
+    description: 'Photo description',
+  };
+  res.send(photo);
+});
+app.listen(port, () => {
+  console.log(`Sunucu ${port} numaralı portta baslatildi`);
+});
